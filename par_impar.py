3539147912990ea0ecc45aead9163fae9f37d41f
Valentin Vladu <79756129+Valentin-new@users.noreply.github.com>
2023-10-27 12:08:02 +0200

    Create par_impar.py
    Compruebo si el numero es par o impar.

---
+++ par_impar.py
@@ -0,0 +1,17 @@
+definimos la función
+def es_par(numero):
+  return numero % 2 == 0
+
+
+def intro_num();
+  #introducimos numero enterto
+  numero= int(inmut("Introduce un numero entero: "))
+
+def arrancar_aplicacion().
+  numero= intro_num()
+  #comprobamos par
+  if es_par(numero):
+      print("el numero", numero, "es par")
+  else:
+      print("el numero", numero, "es impar")
+  
